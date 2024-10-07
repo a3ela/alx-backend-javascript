0ca85bb41fd3424d3432ebd773fa01f0f20937f0
a3ela <abelsintayehu626@gmail.com>
2024-10-07 12:49:15 +0300

    implement pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return ` ${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+}

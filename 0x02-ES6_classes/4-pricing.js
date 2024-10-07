9a2274d7008582168d5d2e560187a746002d0104
a3ela <abelsintayehu626@gmail.com>
2024-10-07 16:06:03 +0300

    added a function coverPrice

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,3 +1,5 @@
+import Currency from './3-currency.js';
+
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
@@ -22,5 +24,10 @@ export default class Pricing {
 
   displayFullPrice() {
     return ` ${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount && typeof conversionRate !== 'number')
+      throw new TypeError('arguments needs to be Numbers');
+    return amount * conversionRate;
   }
 }

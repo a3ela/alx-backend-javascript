9eb2c937d8b2457d6a4c99385da959ec8201d69d
a3ela <abelsintayehu626@gmail.com>
2024-10-07 09:59:59 +0300

    added a comment

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,9 @@
 import ClassRoom from "./0-classroom.js";
 
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
 export default function initializeRooms() {
   let arr = [];
   for (const val of [19, 20, 34]) arr.push(new ClassRoom(val));

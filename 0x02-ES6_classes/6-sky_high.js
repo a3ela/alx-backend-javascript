d7d1c398b3dfecd88c83f0ad77fe56b7acad8e95
a3ela <abelsintayehu626@gmail.com>
2024-10-07 19:29:32 +0300

    extends a building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,34 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(val) {
+    if (typeof val !== 'number') {
+      throw TypeError('sqft needs to be a number');
+    }
+    super._sqft = val;
+  }
+  // getter and setter for floor
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(val) {
+    if (typeof val !== 'number') {
+      throw TypeError('floor needs to be a number');
+    }
+    this._floors = val;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
